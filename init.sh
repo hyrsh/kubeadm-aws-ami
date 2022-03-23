1cb82f436313a85a95f8386756021c810dd794af
root <root@ip-172-31-21-34.eu-central-1.compute.internal>
2022-03-23 10:31:43 +0000

    combat tested generalizing v1

--- init.sh
+++ init.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
+sed -i "s/#Port 22/Port 20234/g" /etc/ssh/sshd_config
+systemctl restart sshd
+echo "Adjusted SSH port to 20234"
+
+echo "Generate RSA ssh keypair only for internal ansible connections"
+ssh-keygen -t rsa -q -f /home/ec2-user/.ssh/id_rsa -N ""
+
+echo "Change required permissions"
+chmod 0600 /home/ec2-user/.ssh/id_rsa*
+
+echo "Add key to have authorized access"
+cat /home/ec2-user/.ssh/id_rsa.pub >> /home/ec2-user/.ssh/authorized_keys
+
+echo "Install Ansible with amazon-extras"
+amazon-linux-extras install ansible2 -y
+
+echo "Install EPEL repository for amazon linux"
+amazon-linux-extras install epel -y
+
+echo "Update YUM package repositories"
 yum update -y
 
-yum install ansible -y
-
 echo "[local]" > /etc/ansible/hosts
-echo "localhost ansible_ssh_private_key_file=/home/ec2-user/.ssh/id_rsa ansible_user=ec2-user" >> /etc/ansible/hosts
+echo "localhost ansible_ssh_private_key_file=/home/ec2-user/.ssh/id_rsa ansible_user=ec2-user ansible_port=20234" >> /etc/ansible/hosts
+echo "Adjusted Ansible hosts"
